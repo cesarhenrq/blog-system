f9d2d152e3fd5f833f430a245462090a1e22a975
cesarhenrq <cesaarhvb@gmail.com>
2023-06-03 06:33:26 -0300

    feat: add post a post route

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 
+import PostModule from "../modules/postModule";
+
 const postsRouter = Router();
+
+const postModule = PostModule.init();
+
+postsRouter.post("/", postModule.create.bind(postModule));
 
 export default postsRouter;
