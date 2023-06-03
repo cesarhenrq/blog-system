893a1fd046f354086cf97fa1155e97e030f41ead
cesarhenrq <cesaarhvb@gmail.com>
2023-06-03 06:31:18 -0300

    feat: add create posts controller

--- src/app/controllers/postsController.ts
+++ src/app/controllers/postsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
+import { object, string } from "yup";
+
 import PostsService from "../services/postsService";
 
 class PostsController {
@@ -7,6 +9,25 @@ class PostsController {
 
   constructor(service: PostsService) {
     this.service = service;
+  }
+
+  async create(req: Request, res: Response) {
+    const { body: payload } = req;
+
+    const payloadSchema = object().shape({
+      content: string().required().min(1).max(280),
+      author: string().required(),
+    });
+
+    try {
+      await payloadSchema.validate(payload);
+    } catch (error: any) {
+      return res.status(400).json({ messages: error.errors, data: null });
+    }
+
+    const { statusCode, message, data } = await this.service.create(payload);
+
+    return res.status(statusCode).json({ message, data });
   }
 }
 
