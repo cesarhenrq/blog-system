c731dc284e0dfeef08c5875e9be5fe58cf2c1421
cesarhenrq <cesaarhvb@gmail.com>
2023-06-03 06:32:49 -0300

    feat: add create posts service

--- src/app/services/postsService.ts
+++ src/app/services/postsService.ts
@@ -1,6 +1,8 @@
 import UsersRepository from "../repositories/usersRepository";
 import PostsRepository from "../repositories/postsRepository";
 
+import { IPost } from "../entities/post";
+
 class PostsService {
   private usersRepository: UsersRepository;
   private postsRepository: PostsRepository;
@@ -11,6 +13,36 @@ class PostsService {
   ) {
     this.usersRepository = usersRepository;
     this.postsRepository = postsRepository;
+  }
+
+  async create(payload: IPost) {
+    try {
+      const { author } = payload;
+
+      const user = await this.usersRepository.findById(author);
+
+      if (!user) {
+        return {
+          statusCode: 404,
+          message: "User not found",
+          data: null,
+        };
+      }
+
+      const post = await this.postsRepository.create(payload);
+
+      return {
+        statusCode: 201,
+        message: "Post created successfully",
+        data: post,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        message: "Internal server error",
+        data: null,
+      };
+    }
   }
 }
 
