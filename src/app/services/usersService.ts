f63261e47b86e9cb69572c9446276fa13ab8af93
cesarhenrq <cesaarhvb@gmail.com>
2023-06-03 06:09:01 -0300

    feat: add create users service

--- src/app/services/usersService.ts
+++ src/app/services/usersService.ts
@@ -1,3 +1,4 @@
+import { IUser } from "../entities/user";
 import UsersRepository from "../repositories/usersRepository";
 
 class UsersService {
@@ -5,6 +6,20 @@ class UsersService {
 
   constructor(repository: UsersRepository) {
     this.repository = repository;
+  }
+
+  async create(payload: IUser) {
+    try {
+      const user = await this.repository.create(payload);
+
+      return {
+        statusCode: 201,
+        message: "User created successfully",
+        data: user,
+      };
+    } catch (error: any) {
+      return { statusCode: 500, message: error.message, data: null };
+    }
   }
 }
 
