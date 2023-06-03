bf200b3bfaaab863a968952bedc76bfe784967ec
cesarhenrq <cesaarhvb@gmail.com>
2023-06-03 06:31:47 -0300

    feat: add create post

--- src/app/repositories/postsRepository.ts
+++ src/app/repositories/postsRepository.ts
@@ -1,10 +1,14 @@
-import Post from "../entities/post";
+import Post, { IPost } from "../entities/post";
 
 class PostsRepository {
   private model: typeof Post;
 
   constructor(model: typeof Post) {
     this.model = model;
+  }
+
+  async create(payload: IPost) {
+    return this.model.create(payload);
   }
 }
 
